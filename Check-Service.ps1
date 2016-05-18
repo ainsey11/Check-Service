b21e526c73cc21d3d3e08e390a8ac3e7001c4f89
Ainsey11 <ainsey11@gmail.com>
2016-05-18 16:03:00 +0100

    The beginning

---
+++ Check-Service.ps1
@@ -0,0 +1,10 @@
+﻿<#    
+  .SYNOPSIS   
+    Checks to see if service is running, if not - restart it, mail me. If service non existant, mail me   
+  .DESCRIPTION   
+    Checks to see if service is running, if not - restart it, mail me. If service non existant, mail me. Uses a foreachloop to find computers 
+  .NOTES 
+  Author: Ainsey11 <https://ainsey11.com>                                      
+  Date : 18/05/2016                           
+#> 
+
