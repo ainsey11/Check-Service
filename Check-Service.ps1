46aad9957265600ba393bfb8b92cac819ac84747
Ainsey11 <ainsey11@gmail.com>
2016-05-25 16:34:51 +0100

    finished, untested though

--- Check-Service.ps1
+++ Check-Service.ps1
@@ -13,7 +13,7 @@
 
 # Other settings
 $ServerFilter = {(Name -Like "THQ*") -And (Enabled -eq "True")}
-$servicename = 
+$servicename = "SolarWindsAgent64"
 $date = Get-Date -Format HH-mm-dd-MMM-yyyy
 $logfile = C:\Scripting\Logs\Check-Service\$date.txt
 
@@ -30,19 +30,29 @@ $servers = Get-ADComputer -Filter $ServerFilter -Properties *
     (foreach $server in $servers)
     {
    
-   if (Get-Service "SolarWindsAgent64")
+   if (Get-Service $servicename)
    {
     Add-Content $logfile "$server has $servicename installed"
+    $CheckRunning = "yes"
     }
     else 
     {
     Add-Content $logfile "$server has not got $servicename installed! Please install"
     Send-MailMessage -SmtpServer $smtpserver -From $smtpfrom -To $smtpto -Subject $smtpsubject -Priority $smtppriority -Body "$servicename does not exist on $server. Please Install to clear error"
+    $checkrunning = "no"
     }
     
+    if ($CheckRunning -eq "yes"){
+        $Service = Get-Service $servicename
+        if ($Service.Status -ne "Started"){
+            Send-MailMessage -SmtpServer $smtpserver -From $smtpfrom -To $smtpto -Subject $smtpsubject -Priority $smtppriority -Body $smtpbody
+            Add-Content $logfile "Service has stopped on $server. Please restart"
+        }
+    else {
+    Add-Content $logfile "Service is running on $server, all is good"
     }
-
-
+    }
+    }
 
 <#
 sudo code - this'll be the end goal
