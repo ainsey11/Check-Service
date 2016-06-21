82203ad6ed89610695e82c0ad134a3c6a04ebacc
Ainsey11 <ainsey11@gmail.com>
2016-06-21 15:27:50 +0100

    Re-made it - it works now.. kinda

--- Check-Service.ps1
+++ Check-Service.ps1
@@ -2,54 +2,44 @@
   .SYNOPSIS   
     Checks to see if service is running, if not - restart it, mail me. If service non existant, mail me   
   .DESCRIPTION   
-    Checks to see if service is running, if not - restart it, mail me. If service non existant, mail me. Uses a foreachloop to find computers 
+    Checks to see if service is running, if not - restart it, mail me. If service non existant, mail me. Uses an AD query to find computers 
   .NOTES 
   Author: Ainsey11 <https://ainsey11.com>                                      
   Date : 18/05/2016                           
 #> 
 
-#Set up my variables
-Import-Module "ActiveDirectory" 
 
-# Other settings
-$ServerFilter = {(Name -Like "THQ*") -And (Enabled -eq "True")}
-$servicename = "SolarWindsAgent64"
-$date = Get-Date -Format HH-mm-dd-MMM-yyyy
-$logfile = C:\Scripting\Logs\Check-Service\$date.txt
+#Importing Modules
+Import-Module ActiveDirectory
 
-# Mail server settings
+# Setting up variables
 
-$smtpserver = "#"
-$smtpfrom = "#"
-$smtpto = "#"
-$smtpsubject = "Ainsey11 Service WatchGuard"
-$smtppriority = "High"
-$smtpbody = "The $servicename service has failed on $failedserver please investigate"
+$ADFilter = {(Operatingsystem -Like "Windows Server*") -And (Enabled -eq "True")}
+$ADServers = Get-ADComputer -Filter $ADFilter -Properties *
+$date = $date = Get-Date -Format HH-mm-dd-MMM-yyyy
+$logfile = "C:\Scripting\Logs\Check-Service\$date"
+$MailServer = ""
+$MailSender = ""
+$MailRecipient = ""
+$MailPriority = ""
+$MailSubject = ""
+$Mailbody = "" 
 
-$servers = Get-ADComputer -Filter $ServerFilter -Properties * 
-   Foreach ($server in $servers)
-    {
-   
-   if (Get-Service $servicename)
-   {
-    Add-Content $logfile "$server has $servicename installed"
-    $CheckRunning = "yes"
-    }
-    else 
-    {
-    Add-Content $logfile "$server has not got $servicename installed! Please install"
-    Send-MailMessage -SmtpServer $smtpserver -From $smtpfrom -To $smtpto -Subject $smtpsubject -Priority $smtppriority -Body "$servicename does not exist on $server. Please Install to clear error"
-    $checkrunning = "no"
-    }
-    
-    if ($CheckRunning -eq "yes"){
-        $Service = Get-Service $servicename
-        if ($Service.Status -ne "Started"){
-            Send-MailMessage -SmtpServer $smtpserver -From $smtpfrom -To $smtpto -Subject $smtpsubject -Priority $smtppriority -Body $smtpbody
-            Add-Content $logfile "Service has stopped on $server. Please restart"
-        }
-    else {
-    Add-Content $logfile "Service is running on $server, all is good"
-    }
-    }
-    }
+
+
+Function LogWrite
+{
+   Param ([string]$logstring)
+
+   Add-content $Logfile -value $logstring
+}
+
+$Status = Invoke-Command -ComputerName $ADServers.name -ScriptBlock {Get-Service -Name "Solarwinds*"} -ErrorVariable $outputerror -ErrorAction SilentlyContinue
+
+$Status  | ForEach-Object {
+if ($_.Status -ne "Running"){
+Write-Host "The Server" $_.PSComputerName "has an issue. Status is currently"$_.Status
+Send-MailMessage -SmtpServer $MailServer -From $MailSender -To $MailRecipient -Priority $MailPriority -Subject $MailSubject -Body $MailBody
+}
+
+}
